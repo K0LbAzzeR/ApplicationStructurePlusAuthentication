23efa8cdfa1679b500779d3c9ef5c621b2c35a3d
K0Lb@zzeR <javasc@yandex.ru>
2023-09-28 13:25:30 +0700

    add Router and Hellow World!

---
+++ app/Services/Router.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+class Router
+{
+    public static function page() : void {
+        echo "Hellow World!";
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
+use \App\Services\Router;
+
 include_once __DIR__ . "/vendor/autoload.php";
+
+Router::page();
